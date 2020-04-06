a4d0418feac950f6fcb1199b2f2b20628ad16949
Yu Okawara <solbonta@hotmail.com>
2020-04-06 23:01:19 +0900

    udemy復習中 PHP復習2 20200406

--- html5/index.php
+++ html5/index.php
@@ -1,2 +1,30 @@
 <?php
-echo "hello world";
+
+$name = "bob";
+echo $name;
+
+$myNumber = 45;
+echo "<p>番号：　$myNumber</p>";
+
+echo "<hr>";
+
+$myarrey = array("bob", "mike", "ken");
+print_r($myarrey);
+
+echo "<br><br>";
+
+
+for($i = 0; $i < 4; $i++){
+    echo $i."<br>";
+}
+echo "<br><br>";
+
+for($i = 2; $i <= 20; $i++){
+    echo $i."<br>";
+}
+echo "<hr>";
+
+$family = array("bob", "mike", "ken");
+foreach ($family as $key => $value){
+    echo $key.":".$value.",";
+}
